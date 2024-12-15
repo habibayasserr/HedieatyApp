c35162939d85fbf775f7680fe4399d535482ca55
habibayasserr <habibahy613@gmail.com>
2024-12-15 02:36:37 +0200

    Added the gifts model

--- lib/models/gift_model.dart
+++ lib/models/gift_model.dart
@@ -1,1 +1,15 @@
+class Gift {
+  final String name;
+  final String category;
+  final String status; // "Available" or "Pledged"
+  final double price;
+  final double description;
 
+  Gift({
+    required this.name,
+    required this.category,
+    required this.status,
+    required this.price,
+    required this.description,
+  });
+}
