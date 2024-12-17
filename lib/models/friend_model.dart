1c16b00766f2247f3e7a6fc41595bd0dd7280a20
habibayasserr <habibahy613@gmail.com>
2024-12-17 15:53:27 +0200

    Added the friend model

--- lib/models/friend_model.dart
+++ lib/models/friend_model.dart
@@ -1,1 +1,25 @@
+class Friend {
+  final int userId; // The user who is adding the friend
+  final int friendUserId; // The friend's user ID (another user)
 
+  Friend({
+    required this.userId,
+    required this.friendUserId,
+  });
+
+  // Convert Friend object to Map
+  Map<String, dynamic> toMap() {
+    return {
+      'user_id': userId,
+      'friend_user_id': friendUserId,
+    };
+  }
+
+  // Convert Map to Friend object
+  factory Friend.fromMap(Map<String, dynamic> map) {
+    return Friend(
+      userId: map['user_id'],
+      friendUserId: map['friend_user_id'],
+    );
+  }
+}
