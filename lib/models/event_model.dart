47982aa879d0e5c899673fcc24757c11e8e1c20a
habibayasserr <habibahy613@gmail.com>
2024-12-18 03:51:03 +0200

    Went back to the updates prior to integrating the local db

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,45 +1,16 @@
 class Event {
-  final int? id;
   final String name;
   final String category;
+  // final String status;
   final DateTime date;
   final String location;
   final String description;
-  final String userId; // Change to String to align with Firebase user ID
 
-  Event({
-    this.id,
-    required this.name,
-    required this.category,
-    required this.date,
-    required this.location,
-    required this.description,
-    required this.userId,
-  });
-
-  // Convert Event to Map for database storage
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'category': category,
-      'date': date.toIso8601String(),
-      'location': location,
-      'description': description,
-      'user_id': userId, // Use string user ID here
-    };
-  }
-
-  // Convert Map to Event for reading from database
-  factory Event.fromMap(Map<String, dynamic> map) {
-    return Event(
-      id: map['id'],
-      name: map['name'],
-      category: map['category'],
-      date: DateTime.parse(map['date']),
-      location: map['location'],
-      description: map['description'],
-      userId: map['user_id'], // Ensure this aligns with string
-    );
-  }
+  Event(
+      {required this.name,
+      required this.category,
+      //required this.status,
+      required this.date,
+      required this.description,
+      required this.location});
 }
